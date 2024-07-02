9bfb1e4bca795358ad7c0f9474cf03dc2712e5c3
gasivelkay <gasivelkay@mtsbank.ru>
2024-07-02 20:03:25 +0300

    HW-18_4 Разграничен доступ к методам контроллера в зависимости от роли пользователя

--- src/main/java/mts/homework/sivelkaev/security/configuration/SecurityConfiguration.java
+++ src/main/java/mts/homework/sivelkaev/security/configuration/SecurityConfiguration.java
@@ -2,11 +2,15 @@ package mts.homework.sivelkaev.security.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
