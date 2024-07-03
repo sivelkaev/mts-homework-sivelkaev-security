047974f1412be0a56c15c3ea0648baa696beefa7
gasivelkay <gasivelkay@mtsbank.ru>
2024-07-03 11:44:55 +0300

    HW-18_6 Реализована аутентификация пользователя с использованием JdbcUserDetailsManager

--- src/main/java/mts/homework/sivelkaev/security/configuration/SecurityConfiguration.java
+++ src/main/java/mts/homework/sivelkaev/security/configuration/SecurityConfiguration.java
@@ -1,18 +1,36 @@
 package mts.homework.sivelkaev.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+    private DataSource dataSource;
+
+    @Autowired
+    public SecurityConfiguration(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    /*@Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
         UserDetails user = User.builder()
                 .username("user")
@@ -27,5 +45,10 @@ public class SecurityConfiguration {
                 .build();
 
         return new InMemoryUserDetailsManager(user, admin);
+    }*/
+
+    @Bean
+    public JdbcUserDetailsManager jdbcUserDetailsManager(PasswordEncoder encoder) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 }
